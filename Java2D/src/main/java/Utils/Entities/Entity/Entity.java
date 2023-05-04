997656614cdc69ea31c02aa0fcbc0ef024685e87
JavierCader <113170731+JavierCader@users.noreply.github.com>
2023-05-04 13:51:35 +0200

    feat: Player Animation

--- Java2D/src/main/java/Utils/Entities/Entity/Entity.java
+++ Java2D/src/main/java/Utils/Entities/Entity/Entity.java
@@ -1,13 +1,17 @@
 package Utils.Entities.Entity;
 
 import Utils.ScreenSettings.ScreenSettings;
+import Utils.Types.Enums.Direction;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public abstract class Entity {
     protected final Point position;
     protected final int speed;
     protected final ScreenSettings screenSettings;
+    protected BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public Direction direction = Direction.DOWN;
 
     public Entity(Point position, int speed, ScreenSettings screenSettings) {
         this.position = position;
--- Java2D/src/main/java/Utils/Entities/Player/Player.java
+++ Java2D/src/main/java/Utils/Entities/Player/Player.java
@@ -7,8 +7,13 @@ package Utils.Entities.Player;
 import Utils.Entities.Entity.Entity;
 import Utils.KeyHandler.KeyHandler;
 import Utils.ScreenSettings.ScreenSettings;
+import Utils.Types.Enums.Direction;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Player extends Entity {
 
@@ -21,6 +26,7 @@ public class Player extends Entity {
      */
     public Player(Point position, int speed, ScreenSettings screenSettings) {
         super(position, speed, screenSettings);
+        getPlayerImages();
     }
 
     /**
@@ -30,15 +36,19 @@ public class Player extends Entity {
      */
     public void updatePosition(KeyHandler keyHandler) {
         if (keyHandler.isUpPressed()) {
+            direction = Direction.UP;
             position.y = Math.max(position.y - speed, 0);
         }
         if (keyHandler.isDownPressed()) {
+            direction = Direction.DOWN;
             position.y = Math.min(position.y + speed, screenSettings.getScreenHeight() - screenSettings.getTileSize());
         }
         if (keyHandler.isLeftPressed()) {
+            direction = Direction.LEFT;
             position.x = Math.max(position.x - speed, 0);
         }
         if (keyHandler.isRightPressed()) {
+            direction = Direction.RIGHT;
             position.x = Math.min(position.x + speed, screenSettings.getScreenWidth() - screenSettings.getTileSize());
         }
     }
@@ -49,7 +59,27 @@ public class Player extends Entity {
      * @param g2d A Graphics2D object used for rendering the player on the screen.
      */
     public void draw(Graphics2D g2d) {
-        g2d.setColor(Color.WHITE);
-        g2d.fillRect(position.x, position.y, screenSettings.getTileSize(), screenSettings.getTileSize());
+        BufferedImage image = switch (direction) {
+            case UP -> (position.y % 30 < 15) ? up1 : up2;
+            case DOWN -> (position.y % 30 < 15) ? down1 : down2;
+            case LEFT -> (position.x % 30 < 15) ? left1 : left2;
+            case RIGHT -> (position.x % 30 < 15) ? right1 : right2;
+        };
+        g2d.drawImage(image, position.x, position.y, screenSettings.getTileSize(), screenSettings.getTileSize(), null);
+    }
+
+    public void getPlayerImages() {
+        try {
+            up1 = ImageIO.read(new File("src/main/resources/Player/up1.png"));
+            up2 = ImageIO.read(new File("src/main/resources/Player/up2.png"));
+            down1 = ImageIO.read(new File("src/main/resources/Player/down1.png"));
+            down2 = ImageIO.read(new File("src/main/resources/Player/down2.png"));
+            left1 = ImageIO.read(new File("src/main/resources/Player/left1.png"));
+            left2 = ImageIO.read(new File("src/main/resources/Player/left2.png"));
+            right1 = ImageIO.read(new File("src/main/resources/Player/right1.png"));
+            right2 = ImageIO.read(new File("src/main/resources/Player/right2.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
